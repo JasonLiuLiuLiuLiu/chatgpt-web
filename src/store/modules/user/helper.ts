8dd0882440dc25e99d9c414879ee75d2b937b4a8
JasonLiu <liuzhenyulive@live.com>
2023-04-13 23:30:37 +0800

    Update default avatar

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      name: 'Jason Liu',
+      description: 'Go to <a href="https://jason.hi.cn" class="text-blue-500" target="_blank" >Hi I\'m Jason</a>',
     },
   }
 }
