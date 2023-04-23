e3d0680b85f8febf8bcee6d80d8d18b2fc48a166
JasonLiu <liuzhenyulive@live.com>
2023-04-23 11:39:25 +0800

    fix error

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      name: 'Jason Liu',
+      description: 'Go to <a href="https://jason.hi.cn" class="text-blue-500" target="_blank" >Hi Jason</a>',
     },
   }
 }
